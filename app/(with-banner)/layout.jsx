a3e7238769d566045afe2ea454291c9924457618
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2023-05-23 01:48:10 -0400

    added a group pages with a same layout :)

--- app/(with-banner)/layout.jsx
+++ app/(with-banner)/layout.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function layout({ children }) {
   return (
     <div>
-      <small>Home &bull; Posts</small>
+      <marquee style={{ background: "purple", border: 4, width: "100%" }}>
+        El mejor canal de &bull; Posts
+      </marquee>
       {children}
     </div>
   );
